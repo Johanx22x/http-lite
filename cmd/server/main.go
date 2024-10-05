1e613d9b4407dc696fd17e94e949d22f7dc84cb6
Johanx22x <johanrodsa2210@gmail.com>
2024-10-04 21:06:27 -0600

    fix body not saved and add more endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"time"
+	"encoding/json"
 
 	"github.com/Johanx22x/http-lite/cmd/server/middleware"
 	"github.com/Johanx22x/http-lite/pkg/http"
@@ -42,6 +43,103 @@ func main() {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte(response))
+		},
+	)
+
+	// Login endpoint (showing how to get a parameter from the URL)
+	mux.AddRoute("/api/login/:id", []string{http.POST},
+		func(w http.ResponseWriter, r *http.Request) {
+			// Get the ID from the URL
+			id := r.Params["id"]
+
+			// Write the response
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"id": "` + id + `"}`))
+		},
+	)
+
+	// Delete method endpoint
+	mux.AddRoute("/api/delete", []string{http.DELETE},
+		func(w http.ResponseWriter, r *http.Request) {
+			body := r.Body
+			defer body.Close()
+
+			// Check if content type is application/json
+			if r.Header.Get("Content-Type") != "application/json" {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "Invalid content type"}`))
+				return
+			}
+
+			// Read the body
+			buf := make([]byte, 1024)
+			n, err := body.Read(buf)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "Internal server error"}`))
+				return
+			}
+
+			// Check if the body is empty
+			if n == 0 {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "Empty body"}`))
+				return
+			}
+
+			// Unmarshal the JSON
+			var data map[string]interface{}
+			err = json.Unmarshal(buf[:n], &data)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "Invalid JSON"}`))
+				return
+			}
+
+			// Check if the ID is present
+			if _, ok := data["id"]; !ok {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "ID not found"}`))
+				return
+			}
+
+			id, ok := data["id"].(string)	
+			if !ok {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Header().Set("Content-Type", "application/json")
+				w.Write([]byte(`{"error": "Invalid ID"}`))
+				return
+			}
+
+			// Write the response
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`Deleted ID: ` + id))
+		},
+	)
+
+	// Put method endpoint
+	mux.AddRoute("/api/update/:id", []string{http.PUT},
+		func(w http.ResponseWriter, r *http.Request) {
+			// Get the ID from the URL
+			id, err := strconv.Atoi(r.Params["id"])
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(`{"error": "Invalid ID"}`))
+				return
+			}
+	
+			newID := strconv.Itoa(rand.Intn(1000) + id)
+
+			// Write the response
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"id": "` + newID + `"}`))
 		},
 	)
 
